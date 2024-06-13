b4834675d8244087c2ea6b6fecad9582c197efd9
backtrackrt5 <backtrackrt5@gmail.com>
2024-06-13 09:35:42 +0900

    현재 내위치를 읽고 맵의 화면을 이동시키기 - 안드로이드, 리액트 모두 완료

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,13 +1,10 @@
 const InfoWindow = ({place}) => {
-    const handleClick = () => {
-        window.history.pushState(null, '', window.location.pathname); // 현재 경로를 이전 페이지로 저장
-        window.location.href = place.place_url; // 새로운 페이지로 이동
-    };
     return (
-        <div onClick={handleClick} style={{userSelect:'none', margin:'.5rem'}}>
+        <div style={{userSelect:'none', margin:'.5rem'}}>
             <h4>{place.place_name}</h4>
             <h5>{place.road_address_name}</h5>
             <h6>{place.phone}</h6>
+            <a href={place.place_url} target="_blank" rel="noopener noreferrer">이동</a>
         </div>
     );
 };
