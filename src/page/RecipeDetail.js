21d065d8ffcca94370b81e92ce90850aeea862fb
LeeYeongMin <ajksl2356@gmail.com>
2024-06-24 16:54:11 +0900

    레시피 디테일에 뒤로가기버튼 그리고수정삭제 구현

--- src/page/RecipeDetail.js
+++ src/page/RecipeDetail.js
@@ -1,19 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import Navigation from "../components/Navigation";
 import styled from 'styled-components';
 import BackBtn from '../components/BackBtn';
 import CommentList from '../components/CommentList';
+import { FaEllipsisV } from "react-icons/fa"
 
 const RecipeDetail = () => {
     const { id } = useParams();
     const [recipe, setRecipe] = useState({});
     const location = useLocation();
+    const navigate = useNavigate()
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Prevent the card click event
+        setShowMenu(!showMenu);
+    }
+    const handleEdit = (e) => {
+        e.stopPropagation(); // Prevent the card click event
+        navigate(`${process.env.PUBLIC_URL}/recipe/detail/:id/${recipe.no}`, { state: { recipe } })
+    }
+    const handleDelete = (e) => {
+        e.stopPropagation(); // Prevent the card click event
+        const userConfirmed = window.confirm("해당 레시피를 삭제하시겠습니까?");
+        if (userConfirmed) {
+            const sendData = new FormData();
+            sendData.append('email', recipe.email);
+            sendData.append('no', recipe.no);
+            fetch(`${process.env.PUBLIC_URL}/backend/recipe_delete.php`, {
+                method: 'POST',
+                body: sendData,
+            })
+                .then(res => res.text())
+                .then(text => {
+                    if (text === "200") {
+                        alert(`레시피를 삭제 하였습니다.`);
+                        navigate('/'); // or wherever you want to navigate after deletion
+                    } else if (text === "201") {
+                        alert(`레시피 삭제를 실패하였습니다.`);
+                    }
+                }).catch(error => console.error('Error:', error));
+        }
+    }
 
     useEffect(() => {
         if (location.state && location.state.recipe) {
             setRecipe(location.state.recipe);
-            if(location.state.recipe.email !== ''){
+            if (location.state.recipe.email !== '') {
                 const sendData = new FormData();
                 sendData.append('email', location.state.recipe.email);
                 sendData.append('myrecipe_id', location.state.recipe.no);
@@ -31,21 +65,52 @@ const RecipeDetail = () => {
                     }).catch(error => console.error('Error:', error));
             } else {
                 // 앱이 아닌 브라우저에서 볼 경우 조회수가 증가되지 않음
-            }   
+            }
         } else {
             // handle case when recipe is not available in location.state
         }
+
     }, [id, location.state]);
 
     if (!recipe || !Object.keys(recipe).length) return <div>Loading...</div>;
 
     return (
         <RecipeDetailContainer>
+            {/* <div>
+                {location.pathname === `/recipe/detail/${id}` && recipe.my_recipe === "1" && (
+                    <>
+                        <MenuIcon onClick={toggleMenu}>
+                            <FaEllipsisV />
+                        </MenuIcon>
+                        {showMenu && (
+                            <DropdownMenu>
+                                <MenuItem onClick={handleEdit}>수정</MenuItem>
+                                <MenuItem onClick={handleDelete}>삭제</MenuItem>
+                            </DropdownMenu>
+                        )}
+                    </>
+                )}
+            </div> */}
             <Navigation />
-            <BackBtn title="레시피 보기" />
-            <ProfileImage src={`${process.env.PUBLIC_URL}/backend/`+recipe.imgfile} alt='프로필'/>
-            <CommentAuthor>{recipe.nickname}</CommentAuthor>
+            <MyModify>
+                <BackBtn title="레시피 보기" />
+                {recipe.my_recipe === "1" && (
+                    <>
+                        <MenuIcon onClick={toggleMenu}>
+                            <FaEllipsisV />
+                        </MenuIcon>
+                        {showMenu && (
+                            <DropdownMenu>
+                                <MenuItem onClick={handleEdit}>수정</MenuItem>
+                                <MenuItem onClick={handleDelete}>삭제</MenuItem>
+                            </DropdownMenu>
+                        )}
+                    </>
+                )}
+            </MyModify>
             
+            <ProfileImage src={`${process.env.PUBLIC_URL}/backend/` + recipe.imgfile} alt='프로필' />
+            <CommentAuthor>{recipe.nickname}</CommentAuthor>
             <RecipeHeader>
                 <h2>{recipe.title}</h2>
             </RecipeHeader>
@@ -80,7 +145,11 @@ const RecipeDetailContainer = styled.div`
     background: #fff;
     padding: 20px;
     box-shadow: 0 0 15px gray;
-`;
+`
+const MyModify = styled.div`
+
+
+`
 
 const ProfileImage = styled.img`
     width: 30px; /* 프로필 이미지 너비 */
@@ -92,6 +161,30 @@ const CommentAuthor = styled.div`
     font-weight: bold;
     margin-bottom: 5px;
     display: inline-block;
+`
+
+const MenuIcon = styled.div`
+  cursor: pointer;
+  font-size: 1em;
+  margin-bottom: 5px;
+`
+
+const DropdownMenu = styled.div`
+  position: absolute;
+  top: 24px;
+  right: 0;
+  background: #fff;
+  border: 1px solid #55a400;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+`
+const MenuItem = styled.div`
+  padding: 8px 16px;
+  cursor: pointer;
+  &:hover {
+    background: #55DD16;
+  }
 `
 
 const RecipeHeader = styled.div`
