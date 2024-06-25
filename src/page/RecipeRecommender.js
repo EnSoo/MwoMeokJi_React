c315e9a3481b3a1a8b0699c33633b126a30ee2da
Dreamtreeme <k99702@gmail.com>
2024-06-25 13:30:30 +0900

    ss

--- src/page/RecipeRecommender.js
+++ src/page/RecipeRecommender.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PreferenceForm from '../components/PreferenceForm';
 import IngredientSearch from '../components/IngredientSearch';
